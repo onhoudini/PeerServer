ba37b181ae063e26bdddebd91f9be664d8599c5b
onhoudini <gabrielrocha.dev@outlook.com>
2025-06-26 18:06:39 -0300

    Correção da porta

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 const { PeerServer } = require('peer');
-const express = require('express');
 
-const app = express();
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 9000; // Render define process.env.PORT
 const PATH = '/meupeers';
-
-// Rota para teste
-app.get('/', (req, res) => {
-  res.send('PeerJS Server está rodando!');
-});
 
 const peerServer = PeerServer({ port: PORT, path: PATH });
 
-app.listen(PORT, () => {
-  console.log(`Servidor PeerJS rodando na porta ${PORT}, path ${PATH}`);
-});
+console.log(`Servidor PeerJS rodando na porta ${PORT}, path ${PATH}`);
